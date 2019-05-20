dd303e578739ea33553e18b74912d2a24946442e
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-05-20 16:16:51 -0400

    Updated ch3 ex
    Updated ch3 ex

--- ch_03/ex_03.R
+++ ch_03/ex_03.R
@@ -8,3 +8,7 @@ library(quantmod)
 
 eqtidx.tickers <- c("CHRIS/HKEX_HSI1", "CHRIS/MX_SXM1")
 eqtidx.raw <- Quandl(cg.tickers, type = "xts")
+
+eqtidx.data <- eqtidx.raw[, grepl('Bid Price', colnames(eqtidx.raw)) | grepl('Ask Price', colnames(eqtidx.raw))]
+
+colnames(cg.data) <- c("CGF1.bid", "CGF1.ask", "CGB1.bid", "CGB1.ask")
