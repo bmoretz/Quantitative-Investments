0f597493c94aaff890bb852115f9b5c869878cfd
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-05-24 07:39:25 -0400

    ch3 ex work
    ch3 ex work

--- ch_03/ex_03.R
+++ ch_03/ex_03.R
@@ -9,9 +9,21 @@ library(quantmod)
 eqtidx.tickers <- c("CHRIS/HKEX_HSI1", "CHRIS/MX_SXM1")
 eqtidx.raw <- Quandl(eqtidx.tickers, type = "xts")
 
-eqtidx.raw <- Quandl(cg.tickers, type = "xts")
+head(eqtidx.raw)
 
-eqtidx.data <- eqtidx.raw[, grepl('Bid Price', colnames(eqtidx.raw)) | grepl('Ask Price', colnames(eqtidx.raw))]
+eqtidx.data <- eqtidx.raw[, grepl('- Bid', colnames(eqtidx.raw)) | grepl('- Ask ', colnames(eqtidx.raw))]
 
-colnames(cg.data) <- c("CGF1.bid", "CGF1.ask", "CGB1.bid", "CGB1.ask")
+colnames(eqtidx.data) <- c("HSI.bid", "HSI.ask", "TSX.bid", "TSX.ask")
 
+eqtidx.data <- eqtidx.data[eqtidx.data[, 1] != 0 & eqtidx.data[, 2] != 0 & eqtidx.data[, 3] != 0 & eqtidx.data[, 4] != 0]
+
+summary(eqtidx.data)
+
+eqtidx.data$HSI.spead <- eqtidx.data$HSI.ask - eqtidx.data$HSI.bid
+eqtidx.data$TSX.spead <- eqtidx.data$TSX.ask - eqtidx.data$TSX.bid
+
+for (v in colnames(eqtidx.data)) {
+  plot.tmp <- plot(eqtidx.data[, v], main = v)
+  print(plot.tmp)
+  invisible(readline(prompt = "Press [enter] to continue"))
+}
