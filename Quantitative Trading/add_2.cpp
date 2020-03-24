2854e81d22acd5c6978950c542f0d1822f7a23a6
Brandon Moretz <bmoretz82@gmail.com>
2020-03-24 16:01:59 -0400

    quant trading, wrap-up
    quant trading, wrap-up

---
+++ Quantitative Trading/add_2.cpp
@@ -0,0 +1,32 @@
+#include <Rcpp.h>
+>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+long add_2_cpp(long max_value) {
+  long sum = 0;
+  for(long i = 0; i <= max_value; ++i) {
+    sum = sum + i;
+  }
+  
+  return sum;
+}
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+timesTwo(42)
+*/
