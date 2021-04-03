06bee41991c7e1fd48dc0671573b5b6142cc30d8
Brandon Moretz <bmoretz82@gmail.com>
2021-04-03 11:56:45 -0400

    factor investing for ml, ch3
    factor investing for ml, ch3

---
+++ Machine Learning for Factor Investing/03_exercises.R
@@ -0,0 +1,13 @@
+library(data.table)
+library(quantmod)
+library(tidyquant)
+library(PerformanceAnalytics)
+library(cowplot)
+library(forecast)
+library(tidyverse)
+library(lubridate)
+library(here)
+
+load(here("Machine Learning for Factor Investing", "data_ml.RData"))
+
+
